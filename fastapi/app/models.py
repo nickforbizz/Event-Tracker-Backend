1228473d75e6383079340f7e56d2a68766288b4f
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-12-06 16:09:40 +0300

    SQL db connection

---
+++ fastapi/app/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Sequence, ForeignKey
+from datetime import datetime
+
+from config.database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True)
+    active = Column(Integer, default = 0)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, Sequence("event_id_seq"), primary_key=True, index=True)
+    name = Column(String(100))
+    description = Column(String(255))
+    fk_user = Column(Integer)
+    active = Column(Integer, default = 0)
+    created_at = Column(DateTime, default=datetime.utcnow)
