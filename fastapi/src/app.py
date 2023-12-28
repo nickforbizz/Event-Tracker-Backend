c0f70d82a0b01b3117b38640783c5575e45eea00
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-12-28 09:10:54 +0300

    User CRUD

--- fastapi/src/app.py
+++ fastapi/src/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from datetime import datetime, timedelta
@@ -12,6 +12,7 @@ from database import engine, SessionLocal
 from model_validators import UserBase, EventBase
 
 app = FastAPI()
+router = APIRouter()
 models.Base.metadata.create_all(bind=engine)
 
 
@@ -24,14 +25,40 @@ def get_db():
         db.close()   
 
 
-@app.get('/', tags=['ROOT'])
+@app.get('/home', tags=['ROOT'])
 def root() -> dict:
     return {'msg': 'Welcome '}
 
+@app.get('/', status_code=status.HTTP_200_OK, tags=['Users'])
+async def get_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = db.query(models.User).offset(skip).limit(limit).all()
+    return users
 
-@app.get('/', status_code=status.HTTP_201_CREATED ,tags=['Users'])
+
+@app.get('/user/{user_id}', status_code=status.HTTP_200_OK ,tags=['Users'])
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return user
+
+@app.post('/user', status_code=status.HTTP_201_CREATED ,tags=['Users'])
 async def createUser(user: UserBase, db: Session=Depends(get_db)):
     user = models.User(**user.model_dump())
     db.add(user)
     db.commit()
     return user
+
+@app.delete('/user/{user_id}', status_code=status.HTTP_204_NO_CONTENT, tags=['Users'])
+async def delete_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    db.delete(user)
+    db.commit()
+
+    return None  # Return None with status_code=204 (No Content)
