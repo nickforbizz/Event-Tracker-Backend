cc181e0f9f9516dda377a179f3997baf1dbef86c
Nicholas Waruingi <NWaruingi@kemri-wellcome.org>
2023-12-31 12:01:43 +0300

    validate event model add model User

--- fastapi/model_validators.py
+++ fastapi/model_validators.py
@@ -1,4 +1,8 @@
-from pydantic import BaseModel, PositiveInt
+from fastapi import Depends
+from pydantic import BaseModel, PositiveInt, validator
+from sqlalchemy.orm import Session
+from src.dependencies import get_db
+from models import User 
 
 class UserBase(BaseModel):
     username: str
@@ -8,3 +12,15 @@ class EventBase(BaseModel):
     name: str
     description: str
     fk_user: PositiveInt
+
+#     @validator("fk_user")
+#     def validate_fk_user_exists(cls, value, values):
+#         db = values.get("db")  # Assuming you pass the database session to the Pydantic model
+#         if not check_user_exists(value):
+#             raise ValueError("User with ID does not exist")
+#         return value
+    
+# def check_user_exists(user_id: int) -> bool:
+#     db: Session = Depends(get_db)
+#     user = db.query(User).filter(User.id == user_id).first()
+#     return user is not None
